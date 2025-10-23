bf0134b23c68dbc7d40f988894cbbb8dc6a28c32
Kurapati Avinash <159903300+aadhiavi@users.noreply.github.com>
2025-10-23 15:51:35 +0530

    Change CORS origin to allow all origins

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://www.tradesyndicate.in',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
   credentials: true
 }));
