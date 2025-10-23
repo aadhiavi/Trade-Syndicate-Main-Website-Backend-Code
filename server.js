f14cc7bcbb837365168d9dfd9bb672db825b638b
Kurapati Avinash <159903300+aadhiavi@users.noreply.github.com>
2025-10-23 15:42:51 +0530

    Update CORS origin to www.tradesyndicate.in

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://tradesyndicate.in',
+  origin: 'https://www.tradesyndicate.in',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
   credentials: true
 }));
