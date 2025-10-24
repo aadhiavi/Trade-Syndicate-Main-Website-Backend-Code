4af28eb304ee21c0bf322ee571a98420383a4551
Kurapati Avinash <aadhiavi57@outlook.com>
2025-10-24 21:18:49 +0530

    testing again

--- config/mailer.js
+++ config/mailer.js
@@ -2,10 +2,9 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    // host: "smtp.gmail.com",
-    // port: 465,
-    // secure: true,
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
